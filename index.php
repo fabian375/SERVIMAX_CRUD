64ee8f02a94cd824bf874c2e70c6384c9c42960a
Claudio Alonso <cealonso@gmail.com>
2021-05-21 12:55:44 -0300

    Visualizar los colores de las zapatilla en el index.html

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 			include_once("php/config_productos.php");
 			include_once("php/database.php");
 			$conn = openConnect();
-			$sql = "select zapatillas.imagen as imagen,marcas.descripcion as descripcion,zapatillas.modelo as modelo, zapatillas.precio as precio,date_format(zapatillas.fecha_alta,'%d/%m/%Y') as fecha from zapatillas,marcas
+			$sql = "select zapatillas.id_zapatilla as id, zapatillas.imagen as imagen,marcas.descripcion as descripcion,zapatillas.modelo as modelo, zapatillas.precio as precio,date_format(zapatillas.fecha_alta,'%d/%m/%Y') as fecha from zapatillas,marcas
 			where zapatillas.id_marca=marcas.id_marca";
 			$result = mysqli_query($conn, $sql);
 			while ($row = mysqli_fetch_assoc($result)) {
@@ -33,14 +33,15 @@
 								<p><?php echo "$" . " " . $row['precio'] ?></p>
 								<time><?php echo $row['fecha'] ?></time>
 								<?php
-
 								$sqlColours = "select m.descripcion as descripcion, z.modelo as modelo, z.precio as precio, z.imagen as imagen, z.fecha_alta as fecha,c.descripcion as color
-								 from zapatillas z,marcas m, colores_zapatillas cz,colores c where z.id_marca=m.id_marca and z.id_zapatilla=cz.id_zapatilla and c.id_color=cz.id_color order by c.descripcion";
-
+								 from zapatillas z,marcas m, colores_zapatillas cz,colores c where z.id_marca=m.id_marca and z.id_zapatilla=cz.id_zapatilla and c.id_color=cz.id_color and z.id_zapatilla=" . $row['id'];
+								$resultColours = mysqli_query($conn, $sqlColours);
+								$textColours = "Colores: ";
+								while ($rowColours = mysqli_fetch_assoc($resultColours)) {
+									$textColours = $textColours . " " . $rowColours['color'];
+								}
 								?>
-
-
-
+								<span><?php echo $textColours ?></span>
 							</figcaption>
 						</figure>
 					</div>
